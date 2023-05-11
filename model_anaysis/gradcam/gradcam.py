14c5bd545134928d5e98d6d86889a9b11b7d9222
brotherhoon <brotherhoon88@gmail.com>
2023-05-11 15:36:34 +0900

    [Rename] LayerExtractor to snake format

--- model_anaysis/gradcam/gradcam.py
+++ model_anaysis/gradcam/gradcam.py
@@ -3,7 +3,7 @@
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
-from LayerExtractor import get_layer
+from layer_extractor import get_layer
 
 class GradCAM(object):
     def __init__(self,
